0c057238d42b1f69741ff93ae8014d21d5adfdca
mohsensh007 <mohsen00711@example.com>
2026-09-24 11:51:36 +0330

    Loading Faster

--- TandisWebApp/Controllers/AccountController.cs
+++ TandisWebApp/Controllers/AccountController.cs
@@ -1,8 +1,9 @@
-using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+using System.Security.Claims;
 using TandisWebApp.DTOs;
 using TandisWebApp.Services;
 
@@ -29,6 +30,7 @@ namespace TandisWebApp.Controllers
         // ============================================================
 
         /// <summary>صفحه لاگین</summary>
+        [EnableRateLimiting("login")]
         [HttpGet]
         public IActionResult Login(string? returnUrl = null)
         {
